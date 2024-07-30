1002d6ad6d2c9b3263f393eaceeb9fc44aec3751
unknown <jeymswork@gmail.com>
2024-07-30 17:15:35 +0800

    updated configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/JAP/',
+  base: '/your-repo-name/', // Replace with your repository name
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
 })
